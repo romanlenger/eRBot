72a9369c9fddf2a13a3d0ff372195b935752df5e
romanlenger <145775490+romanlenger@users.noreply.github.com>
2024-03-21 15:37:58 +0200

    comm3

--- variables.py
+++ variables.py
@@ -9,5 +9,5 @@ url_mb = 'https://minfin.com.ua/ua/currency/mb/'
 url_banks = 'https://minfin.com.ua/ua/currency/banks/'
 
 kurses = r'D:\DALNOBOY\exchange_rates\курсы валют\all'
-update_kurs = r"D:\DALNOBOY\exchange_rates\update_kurs.py"
+update_kurs = r"D:\DALNOBOY\exchange_rates\parser.py"
 interpreter = r"D:\DALNOBOY\exchange_rates\venv\Scripts\python.exe"
