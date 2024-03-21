fc4be469bf849803e641e5158fc8713cde8dfd38
romanlenger <145775490+romanlenger@users.noreply.github.com>
2024-03-21 15:22:10 +0200

    start

---
+++ variables.py
@@ -0,0 +1,13 @@
+url_nbu = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
+
+url_mukachevo = 'https://minfin.com.ua/ua/currency/mukachevo/eur/'
+
+url_mb = 'https://minfin.com.ua/ua/currency/mb/'
+
+url_banks = 'https://minfin.com.ua/ua/currency/banks/'
+
+kurses = r'D:\DALNOBOY\exchange_rates\курсы валют\all'
+update_kurs = r"D:\DALNOBOY\exchange_rates\update_kurs.py"
+interpreter = r"D:\DALNOBOY\exchange_rates\venv\Scripts\python.exe"
