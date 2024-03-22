e0ca8a1be088e5fb48d9c79966ae58b025265daa
romanlenger <145775490+romanlenger@users.noreply.github.com>
2024-03-22 14:29:23 +0200

    Create run.py

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+
+
+async def main() -> None:
+    bot = Bot(token=os.getenv('TOKEN'))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, filename='logs.log', filemode='w', encoding='utf-8')
+    asyncio.run(main())
