49b24bd26d09c3232bdd68eef70837343ad97166
romanlenger <145775490+romanlenger@users.noreply.github.com>
2024-03-22 16:12:06 +0200

    1 handlers.py

--- handlers.py
+++ handlers.py
@@ -1,32 +1,40 @@
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, BoundFilter
 from dotenv import load_dotenv, find_dotenv
-from loginfilter import isLoginned
+import json
 
 load_dotenv(find_dotenv())
 router = Router()
+
+class loginFilter(BoundFilter):
+    key = 'is_loginned'
+
+    def __init__(self, is_login):
+        self.is_login = is_login
+
+    async def check(self, message: types.Message) -> bool:
+        pass
 
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Вітаю, {message.from_user.name}!")
+        user_data = {
+            "id": message.from_user.id,
+            "full_name": message.from_user.full_name
+        }
+        save_user_data(user_data)
 
 
-@router.message(isLoginned(True), Command("get_rates"))
+@router.message(loginFilter(True), Command("get_rates"))
 async def get_rates(message: Message) -> Any:
-    awai message.answer('У вас є доступ до курсу валют!')
+    await message.answer('У вас є доступ до курсів валют!')
 
 
 @router.message()
 async def handle_password(message: Message):
     if message.text == os.getenv('PASSWORD'):
-        user_data = {
-            "id": message.from_user.id,
-            "full_name": message.from_user.full_name
-        }
-        
-        save_user_data(user_data)
 
         await message.answer("Доступ надано.")
     else:
