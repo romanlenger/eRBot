0b82b9f758b3d5b9c61ab87c9efa2d4f2904df97
romanlenger <145775490+romanlenger@users.noreply.github.com>
2024-03-22 16:05:27 +0200

    1 handlers.py

--- handlers.py
+++ handlers.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 from dotenv import load_dotenv, find_dotenv
+from loginfilter import isLoginned
 
 load_dotenv(find_dotenv())
 router = Router()
@@ -9,7 +10,12 @@ router = Router()
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Вітаю, {message.from_user.name}, я щодня о 17:30 надсилаю актульні курси валют!")
+    await message.answer(f"Вітаю, {message.from_user.name}!")
+
+
+@router.message(isLoginned(True), Command("get_rates"))
+async def get_rates(message: Message) -> Any:
+    awai message.answer('У вас є доступ до курсу валют!')
 
 
 @router.message()
