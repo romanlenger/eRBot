5336799b5e22a8cf8c4d517d8aa1ab6a19393229
romanlenger <145775490+romanlenger@users.noreply.github.com>
2024-03-22 14:45:54 +0200

    Create handlers.py

--- handlers.py
+++ handlers.py
@@ -1,5 +1,28 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 router = Router()
+
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Вітаю, {message.from_user.name}, я щодня о 17:30 надсилаю актульні курси валют!")
+    await message.answer("Введіть пароль доступу: ")
+
+
+@router.message()
+async def handle_password(message: Message):
+    if message.text == os.getenv('PASSWORD'):
+        user_data = {
+            "id": message.from_user.id,
+            "full_name": message.from_user.full_name
+        }
+        
+        save_user_data(user_data)
+
+        await message.answer("Доступ надано.")
+    else:
+        await message.answer("Невірний пароль, спробуйте ще раз.")
