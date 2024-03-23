a7910e91cfa2889c35f0309b7f72ea62131d762e
romanlenger <145775490+romanlenger@users.noreply.github.com>
2024-03-23 23:18:48 +0200

    GLOBAL UPDATE 1

--- app/filters.py
+++ app/filters.py
@@ -6,16 +6,19 @@ import os
 
 
 class LoginFilter(Filter):
-    def __init__(self, is_login) -> None:
+    key = 'is_login'
+
+    def __init__(self, is_login):
         self.is_login = is_login
         
     async def __call__(self, message: Message) -> bool:
-        if os.path.exists(r'D:\DALNOBOY\exchange_rates\app\users.json'):
-            with open(r'D:\DALNOBOY\exchange_rates\app\users.json', 'r') as file:
-                users = json.load(file)
-                for user in users:
-                    if user.get('id') == int(message.from_user.id):
-                        return True
-        return False
+        if not os.path.exists(USERS_FILE): return False
+
+        with open(USERS_FILE, 'r') as file:
+            users = json.load(file)
+
+        for user in users:
+            if user.get('id') == int(message.from_user.id):
+                return True
             
 
--- app/handlers.py
+++ app/handlers.py
@@ -2,9 +2,9 @@ from aiogram import Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, FSInputFile
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.filters import CommandStart, Command, Filter
+from aiogram.filters import CommandStart, Command
 
-# from .filters import LoginFilter
+from .filters import LoginFilter
 from dotenv import load_dotenv, find_dotenv
 from .variables import USERS_FILE
 
@@ -19,23 +19,6 @@ router = Router()
 
 class LoginState(StatesGroup):
     waiting_for_password = State()
-    
-
-class LoginFilter(Filter):
-    key = 'is_login'
-
-    def __init__(self, is_login):
-        self.is_login = is_login
-        
-    async def __call__(self, message: Message) -> bool:
-        if not os.path.exists(USERS_FILE): return False
-
-        with open(USERS_FILE, 'r') as file:
-            users = json.load(file)
-
-        for user in users:
-            if user.get('id') == int(message.from_user.id):
-                return True
     
 
 def save_user_data(user_id: int, full_name: str) -> None:
